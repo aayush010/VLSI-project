6b9f60f40812694651f2a390d9df5246f12c1988
aayush010 <jaiswalaayush6@gmail.com>
2020-05-15 19:25:05 +0530

    Add files via upload

---
+++ project13-master/mqdss_48.prj/keypair/csim/build/csim.mk
@@ -0,0 +1,118 @@
+# ==============================================================
+# Vivado(TM) HLS - High-Level Synthesis from C, C++ and SystemC v2019.2 (64-bit)
+# Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+# ==============================================================
+CSIM_DESIGN = 1
+
+__SIM_FPO__ = 1
+
+__HLS_FPO_v6_1__ = 1
+
+__SIM_MATHHLS__ = 1
+
+__SIM_OPENCV__ = 1
+
+__SIM_FFT__ = 1
+
+__SIM_FIR__ = 1
+
+__SIM_DDS__ = 1
+
+ObjDir = obj
+
+HLS_SOURCES = ../../../../newtest_sign.c ../../../../sign.c ../../../../rng.c ../../../../mq.c ../../../../gf31.c ../../../../fips202.c ../../../../aes.c
+
+TARGET := csim.exe
+
+AUTOPILOT_ROOT := C:/Xilinx/Vivado/2019.2
+AUTOPILOT_MACH := win64
+ifdef AP_GCC_M32
+  AUTOPILOT_MACH := Linux_x86
+  IFLAG += -m32
+endif
+ifndef AP_GCC_PATH
+  AP_GCC_PATH := C:/Xilinx/Vivado/2019.2/tps/win64/msys64/mingw64/bin
+endif
+AUTOPILOT_TOOL := ${AUTOPILOT_ROOT}/${AUTOPILOT_MACH}/tools
+AP_CLANG_PATH := ${AUTOPILOT_ROOT}/tps/win64/msys64/mingw64/bin
+AUTOPILOT_TECH := ${AUTOPILOT_ROOT}/common/technology
+
+
+IFLAG += -I "${AUTOPILOT_TOOL}/systemc/include"
+IFLAG += -I "${AUTOPILOT_ROOT}/include"
+IFLAG += -I "${AUTOPILOT_ROOT}/include/opencv"
+IFLAG += -I "${AUTOPILOT_ROOT}/include/ap_sysc"
+IFLAG += -I "${AUTOPILOT_TECH}/generic/SystemC"
+IFLAG += -I "${AUTOPILOT_TECH}/generic/SystemC/AESL_FP_comp"
+IFLAG += -I "${AUTOPILOT_TECH}/generic/SystemC/AESL_comp"
+IFLAG += -I "${AUTOPILOT_TOOL}/auto_cc/include"
+IFLAG += -D__SIM_FPO__
+
+IFLAG += -D__HLS_FPO_v6_1__
+
+IFLAG += -D__SIM_OPENCV__
+
+IFLAG += -D__SIM_FFT__
+
+IFLAG += -D__SIM_FIR__
+
+IFLAG += -D__SIM_DDS__
+
+IFLAG += -D__DSP48E1__
+IFLAG += -Wno-unknown-pragmas 
+IFLAG += -g
+IFLAG += -DNT
+LFLAG += -Wl,--enable-auto-import 
+DFLAG += -D__xilinx_ip_top= -DAESL_TB
+CCFLAG += 
+TOOLCHAIN += 
+
+
+
+include ./Makefile.rules
+
+all: $(TARGET)
+
+
+
+$(ObjDir)/newtest_sign.o: ../../../../newtest_sign.c $(ObjDir)/.dir
+	$(Echo) "   Compiling ../../../../newtest_sign.c in $(BuildMode) mode" $(AVE_DIR_DLOG)
+	$(Verb)  $(CC) ${CCFLAG} -c -MMD -Wno-unknown-pragmas -Wno-unknown-pragmas  $(IFLAG) $(DFLAG) $< -o $@ ; \
+
+-include $(ObjDir)/newtest_sign.d
+
+$(ObjDir)/sign.o: ../../../../sign.c $(ObjDir)/.dir
+	$(Echo) "   Compiling ../../../../sign.c in $(BuildMode) mode" $(AVE_DIR_DLOG)
+	$(Verb)  $(CC) ${CCFLAG} -c -MMD  $(IFLAG) $(DFLAG) $< -o $@ ; \
+
+-include $(ObjDir)/sign.d
+
+$(ObjDir)/rng.o: ../../../../rng.c $(ObjDir)/.dir
+	$(Echo) "   Compiling ../../../../rng.c in $(BuildMode) mode" $(AVE_DIR_DLOG)
+	$(Verb)  $(CC) ${CCFLAG} -c -MMD  $(IFLAG) $(DFLAG) $< -o $@ ; \
+
+-include $(ObjDir)/rng.d
+
+$(ObjDir)/mq.o: ../../../../mq.c $(ObjDir)/.dir
+	$(Echo) "   Compiling ../../../../mq.c in $(BuildMode) mode" $(AVE_DIR_DLOG)
+	$(Verb)  $(CC) ${CCFLAG} -c -MMD  $(IFLAG) $(DFLAG) $< -o $@ ; \
+
+-include $(ObjDir)/mq.d
+
+$(ObjDir)/gf31.o: ../../../../gf31.c $(ObjDir)/.dir
+	$(Echo) "   Compiling ../../../../gf31.c in $(BuildMode) mode" $(AVE_DIR_DLOG)
+	$(Verb)  $(CC) ${CCFLAG} -c -MMD  $(IFLAG) $(DFLAG) $< -o $@ ; \
+
+-include $(ObjDir)/gf31.d
+
+$(ObjDir)/fips202.o: ../../../../fips202.c $(ObjDir)/.dir
+	$(Echo) "   Compiling ../../../../fips202.c in $(BuildMode) mode" $(AVE_DIR_DLOG)
+	$(Verb)  $(CC) ${CCFLAG} -c -MMD  $(IFLAG) $(DFLAG) $< -o $@ ; \
+
+-include $(ObjDir)/fips202.d
+
+$(ObjDir)/aes.o: ../../../../aes.c $(ObjDir)/.dir
+	$(Echo) "   Compiling ../../../../aes.c in $(BuildMode) mode" $(AVE_DIR_DLOG)
+	$(Verb)  $(CC) ${CCFLAG} -c -MMD  $(IFLAG) $(DFLAG) $< -o $@ ; \
+
+-include $(ObjDir)/aes.d
